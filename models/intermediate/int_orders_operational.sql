8c75b3853f7c7413bf2b291311b33fe27a3b5085
louise.beros.beros@gmail.com <louise.beros.beros@gmail.com>
2024-07-24 12:45:55 +0000

    reparation

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,5 +1,6 @@
 SELECT *
-,ROUND((margin_per_order+shipping_fee)-(logcost-ship_cost),2) AS operationel_margin
+,ROUND((margin_per_order+shipping_fee)-(logcost+ship_cost),2) AS operationel_margin
 FROM  {{ ref('int_orders_margin') }}
 JOIN {{ ref('stg_raw__ship') }}
 USING (orders_id)
+ORDER BY orders_id DESC
--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -1,10 +1,16 @@
-SELECT 
-date_date
-,COUNT(orders_id) as nb_transactions
-,ROUND(SUM(revenue_per_order),2) as revenue 
-,ROUND(SAFE_DIVIDE(ROUND(SUM(revenue_per_order),2),COUNT(orders_id)),2) as average_basket
-,ROUND(SUM(margin_per_order),2) as margin
-,ROUND(SUM(operationel_margin),2) as operationnel_margin
+SELECT
+    date_date
+    ,COUNT(orders_id) AS nb_transactions
+    ,ROUND(SUM(revenue_per_order),0) AS revenue 
+    ,ROUND(AVG(revenue_per_order),1) AS average_basket
+    ,ROUND(SUM(revenue_per_order)/COUNT(orders_id),1) AS average_basket_bis
+    ,ROUND(SUM(margin_per_order),0) AS margin 
+    ,ROUND(SUM(operationel_margin),0) AS operational_margin 
+    ,ROUND(SUM(purchase_cost_per_order),0) AS purchase_cost 
+    ,ROUND(SUM(shipping_fee),0) AS shipping_fee 
+    ,ROUND(SUM(logcost),0) AS logcost 
+    ,ROUND(SUM(ship_cost),0) AS ship_cost 
+    ,SUM(qty_per_order) AS quantity 
 FROM {{ ref('int_orders_operational') }}
-GROUP BY date_date 
-ORDER BY date_date DESC
+GROUP BY  date_date
+ORDER BY  date_date DESC
