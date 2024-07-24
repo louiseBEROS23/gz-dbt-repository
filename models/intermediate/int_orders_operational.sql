32c1e9961c5f9c2b9dc68284b7bbe5af4160e901
louiseBEROS23 <louise.beros.beros@gmail.com>
2024-07-24 11:11:34 +0200

    Revert "Louise beros23 patch 1"

--- models/intermediate/int_orders_operational.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-,ROUND((margin_per_order+shipping_fee)-(logcost-ship_cost),2) AS operationel_margin
-FROM  {{ ref('int_orders_margin') }}
-JOIN {{ ref('stg_raw__ship') }}
-USING (orders_id)
--- models/intermediate/int_sales_margin.sql
+++
@@ -1,7 +0,0 @@
-SELECT *
-,ROUND((quantity*purchase_price),2) as purchase_cost
-,ROUND((revenue-(quantity*purchase_price)),2) as margin
-FROM  {{ ref('stg_raw__product')}}
-JOIN {{ ref('stg_raw__sales')}}
-USING (products_id)
-
